ab1f23b0a8a704e458aec9c8ec1b9a7b7781348b
Valentin <birembaut.v@gmail.com>
2015-12-08 22:37:03 +0100

    ajout bouton ascensuer

---
+++ ProjetBoss/Assets/Scripts/Bouton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouton : MonoBehaviour {
+
+	public GameObject ascenseur;
+	PorteAscenseur porte;
+
+	public enum TYPE
+	{
+		OUVERTURE,
+	}
+
+	public TYPE monType;
+
+	// Use this for initialization
+	void Start () {
+		porte = ascenseur.GetComponent<PorteAscenseur>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void utiliserBouton()
+	{
+		if(monType == TYPE.OUVERTURE)
+		{
+			porte.ouverturePorte();
+		}
+	}
+}
